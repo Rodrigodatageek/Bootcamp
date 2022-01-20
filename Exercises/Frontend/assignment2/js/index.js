fbd2ae17c2b8043ff721461cd4021a7fbb6c4690
Rodrigo Fernandes <Rodrigodatageek@gmail.com>
2022-01-20 18:25:41 +0100

    monday update

--- Exercises/Frontend/assignment2/js/index.js
+++
@@ -1,7 +0,0 @@
-let theButton = document.getElementById('theButton');
-let theText = document.querySelectorAll('.top-5__header');    
-theButton.onclick = function () { 
-    for(let x of theText) {
-        x.classList.toggle('top-5__header--colorized');
-    }
-}; 
---
+++ Exercises/Frontend/morn-ex-d9/gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+// compile scss into css
+function style() {
+    // 1. where is my file
+    return gulp.src('./scss/**/*.scss')
+    // 2. pass that file through sass compile
+    .pipe(sass())
+    // 3. where do I save the compiled css?
+    .pipe(gulp.dest('./css'))
+    // 4. stream changes to all browser
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+
+}
+
+exports.style = style;
+exports.watch = watch;
